f33452b9e6db83c1397b67f7cc943f6d6640ccdc
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-08-24 14:34:08 +0300

    chore(): add comments to script files, update README

--- scripts/npm/gh-pages.sh
+++ scripts/npm/gh-pages.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# Build app
 ng build --prod
+
+# Temporary create gh-pages directory and checkout to gh-pages branch in it
 mkdir gh-pages
 cd gh-pages/
 git clone git@github.com:aitarget/aitarget-components.git ./
 git checkout gh-pages
+
+# Update files from current dist directory, commit and push them to github
 cp ../dist/* ./
 git add --all .
 git commit -am "new gh-pages version"
 git push origin +gh-pages
+
+# Remove gh-pages directory
 cd ../
 rm -rf gh-pages/
+
 echo "Deployed! Visit https://aitarget.github.io/aitarget-components/"
 echo "Github pages might take a few minutes to show the deployed site."
