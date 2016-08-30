bd746144b90e9227ff5c8865f7f165b978765c36
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-08-30 21:49:36 +0300

    feature(closable): close detailed-targeting if clicked outside, #36

--- src/app/detailed-targeting/detailed-targeting-mode/detailed-targeting-mode.component.ts
+++ src/app/detailed-targeting/detailed-targeting-mode/detailed-targeting-mode.component.ts
@@ -15,12 +15,39 @@ export class DetailedTargetingModeComponent implements OnInit {
     this.DetailedTargetingModeService.set(mode);
   };
 
-  constructor (private DetailedTargetingModeService: DetailedTargetingModeService,
-               private ref: ChangeDetectorRef) {}
 
-  ngOnInit () {
+  constructor(private DetailedTargetingModeService: DetailedTargetingModeService,
+              private ref: ChangeDetectorRef) {
+  }
+
+  /**
+   * Set mode to null if user click outside detailed-targeting element
+   * @param e
+   */
+  public processOutsideClick = (e) => {
+    console.log('processOutsideClick e.target: ', e.target);
+    let clickedOutside = true;
+    let elm = e.target;
+
+    while (elm && clickedOutside) {
+      clickedOutside = elm.tagName.toLowerCase() !== 'detailed-targeting';
+      elm = elm.parentElement;
+    }
+
+    if (clickedOutside) {
+      this.DetailedTargetingModeService.set(null);
+    }
+  };
+
+  ngOnInit() {
     this.DetailedTargetingModeService.mode.subscribe((mode: string) => {
       this.mode = mode;
+
+      // Process body clicks in order to close element if clicked outside and element
+      window.document.body.removeEventListener('click', this.processOutsideClick);
+      if (mode) {
+        window.document.body.addEventListener('click', this.processOutsideClick);
+      }
 
       this.ref.markForCheck();
       this.ref.detectChanges();
