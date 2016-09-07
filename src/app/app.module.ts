9093ae44a6eff62edf53f373c7c076cda219eb05
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-07 11:39:51 +0300

    refactor(BrowserModule): move BrowserModule from shared/main.module to app.module, #54

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { DetailedTargetingModule } from './detailed-targeting/detailed-targeting.module';
-
 import { AppComponent } from './app.component';
 import { MainModule } from './shared/main.module';
 
@@ -9,11 +8,13 @@ import { MainModule } from './shared/main.module';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports:      [
     DetailedTargetingModule,
+    BrowserModule,
     MainModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/shared/main.module.ts
+++ src/app/shared/main.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    BrowserModule,
     FormsModule
   ],
   exports: [
     CommonModule,
-    BrowserModule,
     FormsModule
   ]
 })
