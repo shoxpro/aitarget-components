80abdd6f582e3326d611efea7c5e930ef57d3992
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-08 18:04:50 +0300

    feature(app): some changes to the app for more advanced demo of detailed-taregting, #62

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:   ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  spec  = {
+    "device_platforms":    [
+      "mobile"
+    ],
+    "publisher_platforms": [
+      "facebook",
+      "instagram",
+      "audience_network"
+    ],
+    "facebook_positions":  [
+      "feed"
+    ],
+    "user_os":             [
+      "iOS"
+    ],
+    "geo_locations":       {
+      "countries": [
+        "RU"
+      ]
+    },
+    "age_min":             18,
+    "age_max":             65,
+    "user_device":         [],
+    "interested_in":       [
+      2
+    ],
+    "interests":           [
+      {
+        "id":   "6002839660079",
+        "name": "Cosmetics"
+      },
+      {
+        "id":   "6002866718622",
+        "name": "Science"
+      }
+    ]
+  };
+
+  onChange = (spec) => {
+    this.spec = spec;
+  };
 }
