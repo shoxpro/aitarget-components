903b05c356874fe09d8dc8c5c3978f9db9b85c4a
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-12 16:38:55 +0300

    fix(remove): fix removing items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,43 +8,70 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   spec  = {
-    "device_platforms":    [
-      "mobile"
+    "age_max": 65,
+    "age_min": 18,
+    "app_install_state": "not_installed",
+    "education_majors": [
+      {
+        "id": "108180979203954",
+        "name": "Economics"
+      }
+    ],
+    "education_schools": [
+      {
+        "id": "112070142203026",
+        "name": "Harvard Medical School"
+      },
+      {
+        "id": "208357009185713",
+        "name": "Stanford Graduate School of Business"
+      },
+      {
+        "id": "268833636515481",
+        "name": "Harobanda"
+      },
+      {
+        "id": "66459568171",
+        "name": "Harvard Business School"
+      }
+    ],
+    "genders": [
+      1
+    ],
+    "geo_locations": {
+      "countries": [
+        "RU"
+      ],
+      "location_types": [
+        "home"
+      ]
+    },
+    "interests": [
+      {
+        "id": "6002839660079",
+        "name": "Cosmetics"
+      },
+      {
+        "id": "6003088846792",
+        "name": "Beauty salons"
+      },
+      {
+        "id": "6003423248519",
+        "name": "Hair care"
+      }
+    ],
+    "user_os": [
+      "iOS"
     ],
     "publisher_platforms": [
       "facebook",
-      "instagram",
-      "audience_network"
+      "instagram"
     ],
-    "facebook_positions":  [
+    "facebook_positions": [
       "feed"
     ],
-    "user_os":             [
-      "iOS"
-    ],
-    "geo_locations":       {
-      "countries": [
-        "RU"
-      ]
-    },
-    "age_min":             18,
-    "age_max":             65,
-    "user_device":         [],
-    //"interested_in":       [
-    //  2
-    //],
-    //"interests":           [
-    //  {
-    //    "id":   "6002839660079",
-    //    "name": "Cosmetics"
-    //  },
-    //  {
-    //    "id":   "6002866718622",
-    //    "name": "Science"
-    //  }
-    //],
-    "education_majors":    [
-      "105540346147170"
+    "device_platforms": [
+      "mobile"
     ]
   };
 
