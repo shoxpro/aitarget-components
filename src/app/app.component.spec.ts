c72228369fc1d8aabf347be0481c88db00e7ee07
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-18 04:34:37 +0300

    test(unit): fix unit tests, #17

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LibModule } from '../lib/lib.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './demo/demo.component';
+import { DemoDetailedTargetingComponent } from './demo-detailed-targeting/demo-detailed-targeting.component';
+import { DemoGeoTargetingComponent } from './demo-geo-targeting/demo-geo-targeting.component';
 
 describe('App: AitargetComponents', () => {
   beforeEach(() => {
@@ -10,8 +15,13 @@ describe('App: AitargetComponents', () => {
       imports:      [
         LibModule
       ],
+      schemas:      [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        DemoComponent,
+        DemoDetailedTargetingComponent,
+        DemoGeoTargetingComponent
       ],
     });
   });
@@ -23,17 +33,24 @@ describe('App: AitargetComponents', () => {
       .toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Aitarget Components'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app     = fixture.debugElement.componentInstance;
     expect(app.title)
-      .toEqual('app works!');
+      .toEqual('Aitarget Components');
   }));
 
-  it('should have toggle button', async(() => {
+  it('should have navbar', async(() => {
     let fixture  = TestBed.createComponent(AppComponent);
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('button').textContent)
-      .toContain('Toggle');
+    expect(compiled.querySelector('app-navbar').tagName)
+      .toContain('APP-NAVBAR');
+  }));
+
+  it('should have router-outlet', async(() => {
+    let fixture  = TestBed.createComponent(AppComponent);
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.app__content router-outlet').tagName)
+      .toContain('ROUTER-OUTLET');
   }));
 });
