504641b7e836bbf0186034344791386b3c2bba53
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-23 13:28:32 +0300

    feature(selected): replace broader and narrower locations when adding new item, #74

--- src/lib/geo-targeting/geo-targeting-selected/geo-targeting-selected.service.ts
+++ src/lib/geo-targeting/geo-targeting-selected/geo-targeting-selected.service.ts
@@ -18,20 +18,29 @@ export class GeoTargetingSelectedService {
 
   public add (item: GeoTargetingItem) {
     let selectedItems = this._items.getValue();
+    let replacedItems = [];
 
-    // Filter out broader region location
-    if (item.region_id) {
-      selectedItems = selectedItems.filter((selectedItem: GeoTargetingItem) => {
-        return selectedItem.key !== item.region_id.toString();
-      });
-    }
+    selectedItems = selectedItems.filter((selectedItem: GeoTargetingItem) => {
+      let toReplace =
+            /*replace selected region, city, zip, geo_market and electoral_district*/
+            selectedItem.country_code === item.key ||
+            /*replace selected country*/
+            selectedItem.key === item.country_code ||
+            /*replace selected region*/
+            (item.region_id && selectedItem.key === item.region_id.toString()) ||
+            /*replace selected city and zip*/
+            (selectedItem.region_id && selectedItem.region_id.toString() === item.key) ||
+            /*replace selected city*/
+            (item.primary_city_id && selectedItem.key === item.primary_city_id.toString()) ||
+            /*replace selected zip*/
+            (selectedItem.primary_city_id && selectedItem.primary_city_id.toString() === item.key);
 
-    // Filter out broader country location
-    if (item.country_code) {
-      selectedItems = selectedItems.filter((selectedItem: GeoTargetingItem) => {
-        return selectedItem.key !== item.country_code;
-      });
-    }
+      if (toReplace) {
+        replacedItems.push(selectedItem);
+      }
+
+      return !toReplace;
+    });
 
     selectedItems.push(item);
 
