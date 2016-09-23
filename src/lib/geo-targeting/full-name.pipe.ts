178547240cb75ca9893751534193ece6ed1cf28a
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-23 13:29:29 +0300

    fix(full-name): don't include country name twice for countries

--- src/lib/geo-targeting/full-name.pipe.ts
+++ src/lib/geo-targeting/full-name.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FullNamePipe implements PipeTransform {
 
   transform (value: any, args?: any): any {
-    let allNames = [value.name, value.region, value.country_name];
+    let allNames = [value.name, value.region, (value.type !== 'country' && value.country_name)];
     return allNames.filter((name) => Boolean(name))
                    .join(', ');
   }
