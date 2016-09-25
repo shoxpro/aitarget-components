3ed0dd7f1f4357994b115a58b3f094dffbd80b46
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-25 23:49:23 +0300

    feat(localization): add translations, fix updating mode when language change, #83

--- src/lib/geo-targeting/geo-targeting-mode/geo-targeting-mode.component.ts
+++ src/lib/geo-targeting/geo-targeting-mode/geo-targeting-mode.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { TranslateService } from 'ng2-translate/ng2-translate';
 import { GeoTargetingModeService } from './geo-targeting-mode.service';
 
 @Component({
-  selector:    'geo-targeting-mode',
-  templateUrl: './geo-targeting-mode.component.html',
-  styleUrls:   ['./geo-targeting-mode.component.css']
+  selector:        'geo-targeting-mode',
+  templateUrl:     './geo-targeting-mode.component.html',
+  styleUrls:       ['./geo-targeting-mode.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GeoTargetingModeComponent implements OnInit, OnDestroy {
 
@@ -14,8 +15,17 @@ export class GeoTargetingModeComponent implements OnInit, OnDestroy {
   public modeTitle;
   public exclude;
 
+  /**
+   * Trigger change detection mechanism that updates component's template
+   */
+  private updateTemplate () {
+    this.ChangeDetectorRef.markForCheck();
+    this.ChangeDetectorRef.detectChanges();
+  }
+
   constructor (private TranslateService: TranslateService,
-               private GeoTargetingModeService: GeoTargetingModeService) { }
+               private GeoTargetingModeService: GeoTargetingModeService,
+               private ChangeDetectorRef: ChangeDetectorRef) { }
 
   public changeMode () {
     let mode = this.GeoTargetingModeService.get();
@@ -39,6 +49,16 @@ export class GeoTargetingModeComponent implements OnInit, OnDestroy {
         this.mode      = mode;
         this.exclude   = mode === 'exclude';
         this.modeTitle = this.TranslateService.instant(`geo-targeting-mode.${mode}`);
+        this.updateTemplate();
+      })
+    );
+
+    /**
+     * Update component's translations on language change
+     */
+    this._subscriptions.push(
+      this.TranslateService.onLangChange.subscribe(() => {
+        this.GeoTargetingModeService.update(this.mode);
       })
     );
   }
