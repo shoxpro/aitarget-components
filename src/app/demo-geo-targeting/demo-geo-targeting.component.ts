cabafd638bd33cd9f7840bb280c5b598a238fde8
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-09-27 12:30:48 +0300

    feat(radius): add saved radius to returned by adgeolocationmeta items, #76

--- src/app/demo-geo-targeting/demo-geo-targeting.component.ts
+++ src/app/demo-geo-targeting/demo-geo-targeting.component.ts
@@ -8,81 +8,87 @@ import { Component, OnInit } from '@angular/core';
 export class DemoGeoTargetingComponent implements OnInit {
 
   spec = {
-    'geo_locations':          {
-      'location_types':      [
-        'home'
-      ],
-      'countries':             ['RU'],
-      /*'regions':             [
-        {
-          'key':  '3153',
-          'name': 'Omsk Oblast'
-        },
-        {
-          'key':  '3146',
-          'name': 'Moscow'
-        }
-      ],
-      'zips':                [
-        {
-          'key':  'RU:192212',
-          'name': '192212'
-        },
-        {
-          'key':  'RU:125222',
-          'name': '125222'
-        },
-        {
-          'key':  'US:13439',
-          'name': '13439'
-        },
-        {
-          'key':  'US:94062',
-          'name': '94062'
-        },
-        {
-          'key':  'US:90028',
-          'name': '90028'
-        }
-      ],
-      'cities':              [
-        {
-          'key':  '2421862',
-          'name': 'San Mateo'
-        },
-        {
-          'key':  '2490299',
-          'name': 'New York'
-        }
-      ],
-      'geo_markets':         [
-        {
-          'key':  'DMA:501',
-          'name': 'New York'
-        },
-        {
-          'key':  'DMA:803',
-          'name': 'Los Angeles'
-        }
-      ],
-      'electoral_districts': [
-        {
-          'key':  'US:CA14',
-          'name': `California's 14th District`
-        }
-      ]*/
-    },
-    'excluded_geo_locations': {
+    'geo_locations': {
       'location_types': [
         'home'
       ],
-      'cities':         [
-        {
-          'key':  '2036122',
-          'name': 'Omsk'
-        }
-      ]
-    }
+      // 'countries':      ['RU'],
+      'cities':         [{
+        'key':           '2020916',
+        'name':          'Moscow',
+        'radius':        50,
+        'distance_unit': 'kilometer'
+      }],
+      /*'regions':             [
+       {
+       'key':  '3153',
+       'name': 'Omsk Oblast'
+       },
+       {
+       'key':  '3146',
+       'name': 'Moscow'
+       }
+       ],
+       'zips':                [
+       {
+       'key':  'RU:192212',
+       'name': '192212'
+       },
+       {
+       'key':  'RU:125222',
+       'name': '125222'
+       },
+       {
+       'key':  'US:13439',
+       'name': '13439'
+       },
+       {
+       'key':  'US:94062',
+       'name': '94062'
+       },
+       {
+       'key':  'US:90028',
+       'name': '90028'
+       }
+       ],
+       'cities':              [
+       {
+       'key':  '2421862',
+       'name': 'San Mateo'
+       },
+       {
+       'key':  '2490299',
+       'name': 'New York'
+       }
+       ],
+       'geo_markets':         [
+       {
+       'key':  'DMA:501',
+       'name': 'New York'
+       },
+       {
+       'key':  'DMA:803',
+       'name': 'Los Angeles'
+       }
+       ],
+       'electoral_districts': [
+       {
+       'key':  'US:CA14',
+       'name': `California's 14th District`
+       }
+       ]*/
+    },
+    /*'excluded_geo_locations': {
+     'location_types': [
+     'home'
+     ],
+     'cities':         [
+     {
+     'key':  '2036122',
+     'name': 'Omsk'
+     }
+     ]
+     }*/
   };
 
   onChange = (spec) => {
