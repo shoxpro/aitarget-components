9a57c9be3b568bf588845e63f327b8763dc6b1e2
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-10-16 23:03:19 +0300

    feat(shared): move shared directives to shared module

--- src/lib/core.module.ts
+++ src/lib/core.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule, TranslateLoader } from 'ng2-translate/ng2-translate';
 import { CustomLoader } from './translate/custom-loader.class';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    SharedModule,
     TranslateModule.forRoot({provide: TranslateLoader, useClass: CustomLoader})
   ],
   exports: [
     CommonModule,
     FormsModule,
+    SharedModule,
     TranslateModule
   ]
 })
