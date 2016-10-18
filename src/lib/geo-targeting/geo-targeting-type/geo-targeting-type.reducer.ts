ea9d8cbcd6d593e2a4bdcfa22efaea4043821726
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-10-18 20:20:10 +0300

    test(geo-targeting-type.reducer): add tests for type reducer, #80 #125

--- src/lib/geo-targeting/geo-targeting-type/geo-targeting-type.reducer.ts
+++ src/lib/geo-targeting/geo-targeting-type/geo-targeting-type.reducer.ts
@@ -1,15 +1,15 @@
-import { Action, ActionReducer } from '@ngrx/store';
+import { ActionReducer } from '@ngrx/store';
 import { SELECT_SEARCH_TYPE, TOGGLE_SEARCH_TYPE_DROPDOWN, TRANSLATE_SEARCH_TYPES } from './geo-targeting-type.actions';
 import { GeoTargetingTypeState } from './geo-targeting-type.interface';
 import { GeoTargetingTypeInitial } from './geo-targeting-type.initial';
 
-export const geoTargetingTypeReducer: ActionReducer<GeoTargetingTypeState> = (state = GeoTargetingTypeInitial, action: Action) => {
-  switch (action.type) {
+export const geoTargetingTypeReducer: ActionReducer<GeoTargetingTypeState> = (state = GeoTargetingTypeInitial, {type, payload}) => {
+  switch (type) {
     case TOGGLE_SEARCH_TYPE_DROPDOWN:
-      return Object.assign({}, state, {isOpen: action.payload.isOpen});
+      return Object.assign({}, state, {isOpen: payload.isOpen});
 
     case SELECT_SEARCH_TYPE:
-      let selectedType = action.payload.selectedType;
+      let selectedType = payload.selectedType;
 
       return Object.assign({}, state, <GeoTargetingTypeState>{
         selectedType: selectedType,
@@ -19,20 +19,20 @@ export const geoTargetingTypeReducer: ActionReducer<GeoTargetingTypeState> = (st
       });
 
     case TRANSLATE_SEARCH_TYPES:
-      const translateService = action.payload.translateService;
-      const translateType    = (type) => {
-        return Object.assign({}, type, {
-          name: translateService.instant(`geo-targeting-dropdown.${type.id}`)
+      const translateService = payload.translateService;
+      const translateType    = (geoType) => {
+        return Object.assign({}, geoType, {
+          name: translateService.instant(`geo-targeting-dropdown.${geoType.id}`)
         });
       };
 
       return Object.assign({}, state, <GeoTargetingTypeState>{
         selectedType: translateType(state.selectedType),
-        selected:     state.selected.map((type) => {
-          return translateType(type);
+        selected:     state.selected.map((geoType) => {
+          return translateType(geoType);
         }),
-        available:    state.available.map((type) => {
-          return translateType(type);
+        available:    state.available.map((geoType) => {
+          return translateType(geoType);
         })
       });
 
