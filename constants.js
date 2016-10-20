1950bf4308b679f2967c61745da25b48059830be
Yuri Malakhov <yuri.malakhov87@gmail.com>
2016-10-20 15:57:29 +0300

    chore(): full path to karma, NPM_CONFIG_PRODUCTION=false to app.json

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ const ip = require('ip');
 exports.HOST = ip.address();
 exports.DEV_PORT = 3000;
 exports.E2E_PORT = 4201;
-exports.PROD_PORT = 8088;
+exports.PROD_PORT = process.env.PORT || 8088;
 
 /**
  * These constants set whether or not you will use proxy for Webpack DevServer
@@ -18,16 +18,16 @@ exports.DEV_SERVER_PROXY_CONFIG = {
 }
 
 /**
- * These constants set the source maps that will be used on build. 
- * For info on source map options, go to: 
+ * These constants set the source maps that will be used on build.
+ * For info on source map options, go to:
  * https://webpack.github.io/docs/configuration.html#devtool
  */
 exports.DEV_SOURCE_MAPS = 'eval';
 exports.PROD_SOURCE_MAPS = 'source-map';
 
 /**
- * Set watch options for Dev Server. For better HMR performance, you can 
- * try setting poll to 1000 or as low as 300 and set aggregateTimeout to as low as 0. 
+ * Set watch options for Dev Server. For better HMR performance, you can
+ * try setting poll to 1000 or as low as 300 and set aggregateTimeout to as low as 0.
  * These settings will effect CPU usage, so optimal setting will depend on your dev environment.
  * https://github.com/webpack/docs/wiki/webpack-dev-middleware#watchoptionsaggregatetimeout
  */
